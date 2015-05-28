b4f4abe6d1fd655e4696b8ec3e5c5798324af98a
sths0614 <sths0614@ucla.edu>
2015-05-28 03:39:38 -0700

    finish ospfs_dir_readdir

--- ospfsmod.c
+++ ospfsmod.c
@@ -452,8 +452,11 @@ ospfs_dir_readdir(struct file *filp, void *dirent, filldir_t filldir)
 		 * the loop.  For now we do this all the time.
 		 *
 		 * EXERCISE: Your code here */
-		r = 1;		/* Fix me! */
-		break;		/* Fix me! */
+
+		if(f_pos>dir_oi->oi_size * OSPFS_DIRENTRY_SIZE){
+			r=1;
+			break;
+		}
 
 		/* Get a pointer to the next entry (od) in the directory.
 		 * The file system interprets the contents of a
@@ -476,6 +479,25 @@ ospfs_dir_readdir(struct file *filp, void *dirent, filldir_t filldir)
 		 */
 
 		/* EXERCISE: Your code here */
+		od = ospfs_inode_data(dir_oi, f_pos*OSPFS_DIRENTRY_SIZE);
+		entry_oi = ospfs_inode(od->od_ino);
+
+		if(entry_oi != 0){
+			switch(entry_oi->oi_ftype){
+				case OSPFS_FTYPE_REG:
+					ok_so_far = filldir(dirent, od->od_name, strlen(od->od_name), f_pos, od->od_ino, DT_REG);
+					break;
+				case OSPFS_FTYPE_DIR:
+					ok_so_far = filldir(dirent, od->od_name, strlen(od->od_name), f_pos, od->od_ino, DT_DIR);
+					break;
+				case OSPFS_FTYPE_SYMLINK:
+					ok_so_far = filldir(dirent, od->od_name, strlen(od->od_name), f_pos, od->od_ino, DT_LNK);
+					break;
+				default:
+					eprintk("Error in dir_readdir");
+			}
+		}
+		f_pos++;
 	}
 
 	// Save the file position and return!
